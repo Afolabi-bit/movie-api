0d27ed492125f259f7b0d67efca76245a6d7ecb8
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-10-07 02:53:59 +0100

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { AppProvider, AppContext } from "./context";
-import { useGlobalContext } from "./context";
+import { AppProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
